24631c6fcfc817225bb9148773424735c8d86617
Arnau Gràcia <agraciataberner02@gmail.com>
2023-01-26 10:56:01 +0100

    init commit with android project

---
+++ ABP1_FireBase_Toni_Arnau/app/src/main/java/com/example/abp1_firebase_toni_arnau/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.abp1_firebase_toni_arnau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
