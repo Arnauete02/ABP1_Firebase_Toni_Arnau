1fc7181bb4455916acb0ff1d13cf573678b44628
Arnau Gràcia <agraciataberner02@gmail.com>
2023-01-31 19:05:18 +0100

    Init changes of code.

--- ABP1_FireBase_Toni_Arnau/app/src/main/java/com/example/abp1_firebase_toni_arnau/view/LoginActivity.java
+++ ABP1_FireBase_Toni_Arnau/app/src/main/java/com/example/abp1_firebase_toni_arnau/view/LoginActivity.java
@@ -3,14 +3,34 @@ package com.example.abp1_firebase_toni_arnau.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.abp1_firebase_toni_arnau.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText email;
+    private EditText password;
+    private Button button_register;
+    private Button button_login;
+    private Button button_google;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        hook();
+
+
+    }
+
+    private void hook(){
+        email = findViewById(R.id.editText_mail);
+        password = findViewById(R.id.editText_Password);
+        button_register = findViewById(R.id.button_register);
+        button_login = findViewById(R.id.button_login);
+        button_google = findViewById(R.id.button_google);
     }
 }
