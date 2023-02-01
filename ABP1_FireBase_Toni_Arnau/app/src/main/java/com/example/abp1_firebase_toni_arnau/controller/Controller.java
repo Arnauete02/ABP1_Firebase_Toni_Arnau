c17e6428ea27bae00514f6fe1a24e7eeabb67395
Arnau Gràcia <agraciataberner02@gmail.com>
2023-02-01 10:34:13 +0100

    Código básico y solución de Merge Error.

--- ABP1_FireBase_Toni_Arnau/app/src/main/java/com/example/abp1_firebase_toni_arnau/controller/Controller.java
+++ ABP1_FireBase_Toni_Arnau/app/src/main/java/com/example/abp1_firebase_toni_arnau/controller/Controller.java
@@ -1,10 +1,12 @@
 package com.example.abp1_firebase_toni_arnau.controller;
 
+import com.example.abp1_firebase_toni_arnau.view.LoginActivity;
 import com.example.abp1_firebase_toni_arnau.view.MainActivity;
 
 public class Controller implements ControllerInterface{
     //Definición de todas las activities como variables globales
     private MainActivity mainActivity;
+    private LoginActivity loginActivity;
 
     //Singleton
     private static Controller controller;
@@ -17,11 +19,17 @@ public class Controller implements ControllerInterface{
     //Se instancia todas las activities en el constructor para prevenir nullPointers
     public Controller() {
         this.mainActivity = new MainActivity();
+        this.loginActivity = new LoginActivity();
     }
 
     public void mainActivity(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
         this.mainActivity.createAllItemsAsGlobalWithGetters();
+    }
+
+    public void loginActivity(LoginActivity loginActivity) {
+        this.loginActivity = loginActivity;
+        this.loginActivity.createAllItemsAsGlobalWithGetters();
     }
 
     @Override
--- ABP1_FireBase_Toni_Arnau/app/src/main/java/com/example/abp1_firebase_toni_arnau/view/LoginActivity.java
+++ ABP1_FireBase_Toni_Arnau/app/src/main/java/com/example/abp1_firebase_toni_arnau/view/LoginActivity.java
@@ -3,34 +3,38 @@ package com.example.abp1_firebase_toni_arnau.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.abp1_firebase_toni_arnau.R;
+import com.example.abp1_firebase_toni_arnau.controller.Controller;
 
-public class LoginActivity extends AppCompatActivity {
-
-    private EditText email;
-    private EditText password;
-    private Button button_register;
-    private Button button_login;
-    private Button button_google;
+public class LoginActivity extends AppCompatActivity implements ViewActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        setTheme(R.style.Theme_ABP1_FireBase_Toni_Arnau);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        hook();
-
-
+        callControllerWithThisActivityAsParameter();
     }
 
-    private void hook(){
-        email = findViewById(R.id.editText_mail);
-        password = findViewById(R.id.editText_Password);
-        button_register = findViewById(R.id.button_register);
-        button_login = findViewById(R.id.button_login);
-        button_google = findViewById(R.id.button_google);
+    @Override
+    public void callControllerWithThisActivityAsParameter() {
+        Controller.getInstance().loginActivity(this);
+    }
+
+    @Override
+    public void createAllItemsAsGlobalWithGetters() {
+
     }
 }
