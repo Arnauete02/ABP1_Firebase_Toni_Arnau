90968f6b1e0440a68ba352a439196cecdfef9906
tcp <tcp.new.id@gmail.com>
2023-02-06 18:03:13 +0100

    Home view/Activity Done!! Eliminar fichero SharedPreferences

--- ABP1_FireBase_Toni_Arnau/app/src/main/java/com/example/abp1_firebase_toni_arnau/controller/Controller.java
+++ ABP1_FireBase_Toni_Arnau/app/src/main/java/com/example/abp1_firebase_toni_arnau/controller/Controller.java
@@ -1,5 +1,8 @@
 package com.example.abp1_firebase_toni_arnau.controller;
 
+import android.view.View;
+import android.widget.Button;
+
 import com.example.abp1_firebase_toni_arnau.ExtraActivity;
 import com.example.abp1_firebase_toni_arnau.view.AhorcadoActivity;
 import com.example.abp1_firebase_toni_arnau.view.EstadisticasActivity;
@@ -8,6 +11,7 @@ import com.example.abp1_firebase_toni_arnau.view.LoginActivity;
 import com.example.abp1_firebase_toni_arnau.view.MainActivity;
 import com.example.abp1_firebase_toni_arnau.view.ParaulogicActivity;
 import com.example.abp1_firebase_toni_arnau.view.PerfilActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Controller implements ControllerInterface{
     //Definición de todas las activities como variables globales
@@ -54,6 +58,7 @@ public class Controller implements ControllerInterface{
     public void homeActivity(HomeActivity homeActivity) {
         this.homeActivity = homeActivity;
         this.homeActivity.createAllItemsAsGlobalWithGetters();
+        createActivityButtons();
     }
 
     public void ahorcadoActivity(AhorcadoActivity ahorcadoActivity) {
@@ -83,6 +88,52 @@ public class Controller implements ControllerInterface{
 
     @Override
     public void createActivityButtons() {
+        this.homeActivity.getBotonLogout().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                switchActivity(homeActivity, loginActivity);
+                //Eliminar fichero SharedPreferences
+            }
+        });
+
+        this.homeActivity.getBotonAhorcado().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivity(homeActivity, ahorcadoActivity);
+            }
+        });
+
+        this.homeActivity.getBotonLetras().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivity(homeActivity, extraActivity);
+            }
+        });
+
+        this.homeActivity.getBotonPalabra().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivity(homeActivity, paraulogicActivity);
+            }
+        });
+
+        this.homeActivity.getBotonPeril().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivity(homeActivity, perfilActivity);
+            }
+        });
+
+        this.homeActivity.getBotonStats().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivity(homeActivity, estadisticasActivity);
+            }
+        });
+
+
+
 
     }
 }
